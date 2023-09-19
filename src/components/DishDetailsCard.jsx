10b981a973fae6ebdabc1fcb84d68795860eb80d
James Yuill <jamesyuill@jamess-mbp.lan>
2023-09-19 11:21:24 +0100

    adds carb images to the dishdetailscard

--- src/components/DishDetailsCard.jsx
+++ src/components/DishDetailsCard.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react';
+
 export default function DishDetailsCard({ selectedDish }) {
+  let imgSrc = '';
+
+  switch (selectedDish.carbType) {
+    case 'Pasta':
+      imgSrc = 'pasta.jpg';
+      break;
+    case 'Rice':
+      imgSrc = 'rice.jpg';
+      break;
+    case 'Noodles':
+      imgSrc = 'noodles.jpg';
+      break;
+    case 'Cous Cous':
+      imgSrc = 'couscous.jpg';
+      break;
+    case 'Potato':
+      imgSrc = 'potato.jpg';
+      break;
+    case 'Tortilla':
+      imgSrc = 'tortilla.jpg';
+      break;
+  }
+
   return (
     <div className="dish-details-card">
       <div className="dish-title">{selectedDish.title}</div>
       <div className="dish-image">
-        <img src="/images/pesto-pasta.jpg" />
+        <img src={`/images/${imgSrc}`} />
       </div>
       <div className="dish-ingredients">
         Ingredients:
